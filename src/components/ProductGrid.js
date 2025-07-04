0d1453a2ef7ae43b1b568c932dcaf21523457572
AYOUBgmr <ayougmr@gmail.com>
2025-07-04 14:11:00 +0100

    رسالة توضح التعديلات التي قمت بها

--- src/components/ProductGrid.js
+++ src/components/ProductGrid.js
@@ -2,14 +2,11 @@ import ProductCard from "./ProductCard.js";
 
 const ProductGrid = ({ products, addToCart }) => (
   <div className="product-grid">
-    {products.length === 0 ? (
-      <p style={{ padding: "20px" }}>لا توجد منتجات مطابقة.</p>
-    ) : (
-      products.map((product, index) => (
-        <ProductCard key={index} product={product} addToCart={addToCart} />
-      ))
-    )}
+    {products.map((product, index) => (
+      <ProductCard key={index} product={product} addToCart={addToCart} />
+    ))}
   </div>
 );
 
 export default ProductGrid;
+
